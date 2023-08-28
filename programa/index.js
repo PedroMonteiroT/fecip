b66fa51ea6a19626f89e649728a313db4737bed6
PedroDaAlanis <pmtex06@gmail.com>
2023-08-28 00:58:48 -0300

    mdsdoceu

--- programa/index.js
+++ programa/index.js
@@ -1,14 +1,10 @@
-function search(){
-    let input = document.getElementById('searchbar').value;
-    input = input.toLowerCase();
-    let x = document.getElementsByClassName('muscles');
+window.sr = ScrollReveal({ reset: true});
+sr.reveal('#logo', {duration: 3000})
+sr.reveal('#h1art', {duration: 1000})
+sr.reveal('.caixa', {duration: 1000})
 
-    for(i = 0; i < x.length; i++){
-        if(!x[i].innerHTML.toLowerCase().includes(input)){
-            x[i].style.display = "none";
-        }else{
-            x[i].style.display = "list-item"
-        }
-    }
-}
+
+
+
+
 
