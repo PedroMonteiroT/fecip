39a9976df94560fc668894325f9db3150ab91575
PedroDaAlanis <pmtex06@gmail.com>
2023-09-10 10:07:15 -0300

    site novim bb

--- programa/index.js
+++ programa/index.js
@@ -1,38 +1,15 @@
-window.sr = ScrollReveal({ reset: true});
-sr.reveal('#logo', {duration: 3000})
-sr.reveal('#h1art', {duration: 1000})
+let count = 1;
+document.getElementById("radio1").checked = true;
 
-function aparecer(){
-    const caixa1 = document.getElementById('caixa1');
-const caixa2 = document.getElementById('caixa2');
-const caixa3 = document.getElementById('caixa3');
-const caixas = [caixa1, caixa2, caixa3];
+setInterval(function () {
+  nextImage();
+}, 6000);
 
-function closeOtherBoxes(clickedBox) {
-    caixas.forEach(caixa => {
-        if (caixa !== clickedBox && caixa.style.transform === 'translateX(0px)') {
-            caixa.style.transform = 'translateX(570px)';
-        }
-    });
+function nextImage() {
+  count++;
+  if (count > 4) {
+    count = 1;
+  }
+  document.getElementById("radio" + count).checked = true;
 }
-
-function toggleTranslate(element) {
-    if (element.style.transform === 'translateX(0px)') {
-        element.style.transform = 'translateX(570px)';
-    } else {
-        closeOtherBoxes(element);
-        element.style.transform = 'translateX(0)';
-    }
-}
-
-caixas.forEach(caixa => {
-    caixa.addEventListener('click', () => {
-        toggleTranslate(caixa);
-    });
-});
-
-}
-
-
-
 
