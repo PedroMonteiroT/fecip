c823db4e15332a92cf323a3d6c7f16b58903eace
PedroDaAlanis <pmtex06@gmail.com>
2023-09-28 10:53:57 -0300

    tentando cadastrar

--- cadastro.php
+++ cadastro.php
@@ -1,33 +1,26 @@
 <?php
-$servername = "Local instance MySQL80"; // Nome do servidor MySQL (geralmente é "localhost")
-$port = "3306"; // Porta do servidor MySQL (geralmente é 3306)
-$database = "cadastro"; // Nome do banco de dados
-$username = "root"; // Nome de usuário do MySQL
-$password = ""; // Senha do MySQL (deixe em branco se não houver senha)
 
-$conn = new mysqli($servername, $username, $password, $database, $port);
 
+$servername = "localhost";
+$database = "cadastro";
+$username = "root";
+$password = "";
+//Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+//Check connection
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
-
-$nomeusuario = $_POST['nomeusuario'];
-$senha = $_POST['senha'];
-$repsenha = $_POST['repsenha'];
+$nomeusuario        = $_POST['nomeusuario'];
+$senha              = $_POST['senha'];
+$repsenha           = $_POST['repsenha'];
 
 if ($senha != $repsenha) {
-    header('Location: cadastro.html'); // Redireciona de volta para o formulário de cadastro
-    exit;
+    Echo "<script>JavaScript:alert('Cadastro Negado')</script>";
+    header('Location:cadastro.html');
 }
-
-$query = "INSERT INTO usuarios (nome, senha) VALUES ('$nomeusuario', '$senha')";
-
-if ($conn->query($query) === TRUE) {
-    header('Location: main.html'); // Redireciona para a página "main.html" após o cadastro bem-sucedido
-    exit;
-} else {
-    echo "Erro ao cadastrar: " . $conn->error;
-}
-
-$conn->close();
+$query = "insert into usuarios(nome, senha) values ('".$nomeusuario."', '".$senha."')";
+mysqli_query($conn, $query);
+echo "<script>javascript:alert('Cadastrado com sucesso')</script>";
+header('Location:main.html');
 ?>
